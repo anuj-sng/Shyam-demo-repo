4499a821997e8c32af75694190021559e88ce35e
anuj-sng <134609593+anuj-sng@users.noreply.github.com>
2023-05-25 22:00:32 +0530

    Create V1.1.3__initial_objects.sql

---
+++ migrations/V1.1.3__initial_objects.sql
@@ -0,0 +1,6 @@
+insert into e2e_dw.dummy values (1);
+insert into e2e_dw.dummy values (2);
+insert into e2e_dw.dummy values (3);
+insert into e2e_dw.dummy2 values (11, 'NAME1');
+insert into e2e_dw.dummy2 values (22,'NAME2');
+insert into e2e_dw.dummy2 values (33,'NAME3');
