9f26be52b07459dd0711ab0f38a3f2045033fb57
anuj-sng <134609593+anuj-sng@users.noreply.github.com>
2023-05-26 16:42:22 +0530

    Create V1.1.4_initial_objects.sql

---
+++ migrations/V1.1.4_initial_objects.sql
@@ -0,0 +1,21 @@
+ALTER TABLE ADOPTION SET COMMENT = "Adoption table contains project & experiment that are selected for further process";
+
+create or replace view E2E_DEV_DB.E2E_DW.CURRCNVRSN_VW(
+	FROMCURRID,
+	TOCURRID,
+	EXCHANGERATE ,
+	FROM_DT,
+	TO_DT COMMENT 'CURRENCY CONVERSION RATE'  
+) as 
+SELECT 
+                 FROMCURRID
+                ,TOCURRID
+                ,EXCHANGERATE
+                 , DATE AS FROM_DT
+                ,NVL(LEAD(DATE-1,1) OVER(PARTITION BY FROMCURRID, TOCURRID ORDER BY DATE ),'9999-12-31')  AS TO_DT 
+FROM ( SELECT FROMCURRID, TOCURRID,EXCHANGERATE,DATE
+                                FROM CURRCNVRSN CURR
+                                     ,CALENDAR CAL 
+                                WHERE CURR.EXCHANGEEFFECTIVDATEID = CAL.DATEID
+                                  AND EXCHANGERATETYPEID  = (SELECT CURREXCHANGERATETYPEID   FROM CURREXCHANGERATETYPE WHERE CURREXCHANGERATETYPE ='001S'
+                                  )         );
