544d9266d8c2d31b4943ad9032102b179a1900e7
anuj-sng <134609593+anuj-sng@users.noreply.github.com>
2023-05-26 17:31:41 +0530

    Update V1.1.1__initial_objects.sql

--- migrations/V1.1.1__initial_objects.sql
+++ migrations/V1.1.1__initial_objects.sql
@@ -1,29 +1,21 @@
-ALTER TABLE SUBMISSIN ADD  CONSTRAINT submissin_projectFK FOREIGN KEY ( PROJECTID )
-  REFERENCES PROJECT (PROJECTID);---- Referential intigretiy
- 
- 
-ALTER TABLE ADOPTION SET COMMENT = "Adoption table contains project & experiment that are selected for further process";-- COMMENTS IN TABLE
+ALTER TABLE E2E_DW_UAT.ADOPTION SET COMMENT = "Adoption table contains project & experiment that are selected for further process";
 
---COMMENTS IN VIEW--
-create or replace view E2E_DEV_DB.E2E_DW.CURRCNVRSN_VW(
-      FROMCURRID,
-      TOCURRID,
-      EXCHANGERATE ,
-      FROM_DT,
-      TO_DT COMMENT 'CURRENCY CONVERSION RATE'  
-) as
-SELECT
+create or replace view E2E_DW_UAT.CURRCNVRSN_VW(
+	FROMCURRID,
+	TOCURRID,
+	EXCHANGERATE ,
+	FROM_DT,
+	TO_DT COMMENT 'CURRENCY CONVERSION RATE'  
+) as 
+SELECT 
                  FROMCURRID
                 ,TOCURRID
                 ,EXCHANGERATE
                  , DATE AS FROM_DT
-                ,NVL(LEAD(DATE-1,1) OVER(PARTITION BY FROMCURRID, TOCURRID ORDER BY DATE ),'9999-12-31')  AS TO_DT
+                ,NVL(LEAD(DATE-1,1) OVER(PARTITION BY FROMCURRID, TOCURRID ORDER BY DATE ),'9999-12-31')  AS TO_DT 
 FROM ( SELECT FROMCURRID, TOCURRID,EXCHANGERATE,DATE
                                 FROM CURRCNVRSN CURR
-                                     ,CALENDAR CAL
+                                     ,CALENDAR CAL 
                                 WHERE CURR.EXCHANGEEFFECTIVDATEID = CAL.DATEID
-                                  AND EXCHANGERATETYPEID  = (SELECT CURREXCHANGERATETYPEID
-                                                                                FROM CURREXCHANGERATETYPE
-                                                                                 WHERE CURREXCHANGERATETYPE ='001S'
-                                                                                )
-                );
+                                  AND EXCHANGERATETYPEID  = (SELECT CURREXCHANGERATETYPEID   FROM CURREXCHANGERATETYPE WHERE CURREXCHANGERATETYPE ='001S'
+                                  )         );
