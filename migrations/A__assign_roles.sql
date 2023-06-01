87a9fdd42514f053f7bf1ca47ae1e7a11933d0e3
Shyam <134609593+anuj-sng@users.noreply.github.com>
2023-06-01 13:32:35 +0530

    Update A__assign_roles.sql

--- migrations/A__assign_roles.sql
+++ migrations/A__assign_roles.sql
@@ -1,5 +1,6 @@
-ALTER TABLE E2E_DW_UAT.ADOPTION SET COMMENT = "Adoption tables contains project & experiment that are selected for further process";
 """
+ALTER TABLE E2E_DW_UAT.ADOPTION SET COMMENT = 'Adoption tables contains project & experiment that are selected for further process';
+
 create or replace view E2E_DW_UAT.CURRCNVRSN_VW(
 	FROMCURRID,
 	TOCURRID,
